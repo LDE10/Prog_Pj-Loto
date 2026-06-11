2280cf08d8e0fd211383e275e8e9dd5da6dba6ef
Luc Derré <luc.derre@eduvaud.ch>
2026-06-11 14:44:06 +0200

    Update GestionLoto.h

--- Prog_Pj-Loto_LDE-LMY-CAH/Prog_Pj-Loto_LDE-LMY-CAH/GestionLoto.h
+++ Prog_Pj-Loto_LDE-LMY-CAH/Prog_Pj-Loto_LDE-LMY-CAH/GestionLoto.h
@@ -1,10 +1,10 @@
 //-----------------------------------------------------------------------------------//
 // Nom du projet 		: ProjetLoto
 // Nom du fichier 		: GestionValeurlotoGagnante.h
-// Date de cr�ation 	: 18.05.2026
+// Date de création 	: 18.05.2026
 // Date de modification : 18.05.2026
 //
-// Auteur 				: LMY (Lo�c Marmy)
+// Auteur 				: LMY (Loïc Marmy)
 //
 // Description          : programme principale 
 //
@@ -23,8 +23,51 @@ typedef struct
     int* tbLeastWinning;
     int* tbValue;
     int* tbWin;
-    int** tbHistoTirage; // Tableau dynamique � 2 dimensions [lignes][colonnes]
-    int nbHistoRows;     // Nombre de tirages stock�s dans l'historique
+    int** tbHistoTirage; // Tableau dynamique à 2 dimensions [lignes][colonnes]
+    int nbHistoRows;     // Nombre de tirages stockés dans l'historique
+} str_Value;
+
+extern str_Value Value;
+
+
+void ExistingFile();
+void ConfigFile();
+char ModeLoto(char* Mode);
+void ChoiceValue(int* tbValue, char Mode, char NbCompl, str_Rules* r);
+int Tirage(str_Rules* d, int* tbTirage, int LastVal);
+void Win(str_Value* v, str_Rules* r);
+void RecupHisto(str_Value* v, FILE* fp1, int nbNumbers, char InputUser);
+void AddTirageToHisto(str_Value* v, int nbNumbers);
+void SaveAllData(str_Rules* r, str_Value* v, FILE* fp1, int nbNumbers);
+void freetb();
+char Input();
+#endif//-----------------------------------------------------------------------------------//
+// Nom du projet 		: ProjetLoto
+// Nom du fichier 		: GestionValeurlotoGagnante.h
+// Date de création 	: 18.05.2026
+// Date de modification : 18.05.2026
+//
+// Auteur 				: LMY (Loïc Marmy)
+//
+// Description          : programme principale 
+//
+//
+// Remarques			: lien pour les lib standard:
+//						-> https://www.rocq.inria.fr/secret/Anne.Canteaut/COURS_C/annexe.html
+//						-> 
+//
+//----------------------------------------------------------------------------------//
+#ifndef GESTIONLOTO_H
+#define GESTIONLOTO_H
+typedef struct
+{
+    int* tbTirage;
+    int* tbMostWinning;
+    int* tbLeastWinning;
+    int* tbValue;
+    int* tbWin;
+    int** tbHistoTirage; // Tableau dynamique à 2 dimensions [lignes][colonnes]
+    int nbHistoRows;     // Nombre de tirages stockés dans l'historique
 } str_Value;
 
 extern str_Value Value;
